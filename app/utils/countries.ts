8eff98f3eeb25dae69df5dc76ed2a01d14445f97
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-05-15 21:27:54 +0200

    feat: ECOM-40 country list ISO 3166-1 Alpha-2 code helper

---
+++ app/utils/countries.ts
@@ -0,0 +1,6 @@
+import { registerLocale, getNames } from 'i18n-iso-countries'
+import countryNames from 'i18n-iso-countries/langs/en.json'
+
+registerLocale(countryNames)
+
+export const countries = getNames('en', { select: 'official' })
