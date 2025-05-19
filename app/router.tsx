f605ca4d0a36115445334a17fccf6ea377c7e5db
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-05-19 11:18:35 +0200

    feat: ECOM-40 check auth on page load

--- app/router.tsx
+++ app/router.tsx
@@ -1,9 +1,13 @@
-import { lazy, Suspense, type ReactElement } from 'react'
+import { lazy, Suspense, type ReactElement, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
-import Loading from '~/components/loading'
+import { Loading } from '~/components/loading'
+import { Toaster } from '~/components/ui/sonner'
 
 import PublicLayout from '~/layouts/public'
 import ProtectedLayout from '~/layouts/protected'
+
+import { useAppDispatch } from '~/store/hooks'
+import { checkAuth } from '~/store/auth'
 
 const Home = lazy(() => import('~/pages/home'))
 const Catalog = lazy(() => import('~/pages/catalog'))
@@ -16,28 +20,36 @@ const Cart = lazy(() => import('~/pages/cart'))
 const NotFound = lazy(() => import('~/pages/not-found'))
 
 export function Router(): ReactElement {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => void dispatch(checkAuth()), [dispatch])
+
   return (
-    <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route element={<PublicLayout />}>
-            <Route index element={<Home />} />
-            <Route path="catalog" element={<Catalog />} />
-            <Route path="product/:productId" element={<Product />} />
-            <Route path="about" element={<About />} />
-            <Route path="auth">
-              <Route index element={<Navigate to="login" replace />} />
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
+    <>
+      <BrowserRouter>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route element={<PublicLayout />}>
+              <Route index element={<Home />} />
+              <Route path="catalog" element={<Catalog />} />
+              <Route path="product/:productId" element={<Product />} />
+              <Route path="about" element={<About />} />
+              <Route path="auth">
+                <Route index element={<Navigate to="login" replace />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+              </Route>
             </Route>
-          </Route>
-          <Route element={<ProtectedLayout />}>
-            <Route path="profile" element={<Profile />} />
-            <Route path="cart" element={<Cart />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+            <Route element={<ProtectedLayout />}>
+              <Route path="profile" element={<Profile />} />
+              <Route path="cart" element={<Cart />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+
+      <Toaster />
+    </>
   )
 }
--- app/store/auth/index.ts
+++ app/store/auth/index.ts
@@ -1,10 +1,11 @@
-import { createAppSlice } from '~/store/hooks'
 import { type Customer } from '@commercetools/platform-sdk'
+import { createSelector } from '@reduxjs/toolkit'
+import { type RootState } from '~/store'
+import { createAppSlice } from '~/store/hooks'
 import { createSignInThunk } from './reducers/sign-in'
 import { createSignUpThunk } from './reducers/sign-up'
 import { createLogOutThunk } from './reducers/log-out'
-import { createSelector } from '@reduxjs/toolkit'
-import type { RootState } from '..'
+import { createCheckAuthThunk } from './reducers/check-auth'
 
 export enum AUTH_STATUS {
   LOADING = 'LOADING',
@@ -29,9 +30,15 @@ const auth = createAppSlice({
   initialState,
 
   reducers: (create) => ({
+    checkAuth: createCheckAuthThunk(create),
+    logOut: createLogOutThunk(create),
     signIn: createSignInThunk(create),
     signUp: createSignUpThunk(create),
-    logOut: createLogOutThunk(create)
+
+    resetAuthError: create.reducer((state) => {
+      state.errorMessage = ''
+      state.status = AUTH_STATUS.READY
+    })
   })
 })
 
@@ -39,5 +46,5 @@ const selectAuthSlice = (state: RootState): AuthState => state.auth
 
 export const selectIsAuth = createSelector([selectAuthSlice], (auth) => auth.customer !== undefined)
 
-export const { signIn, signUp, logOut } = auth.actions
+export const { signIn, signUp, logOut, checkAuth, resetAuthError } = auth.actions
 export default auth.reducer
