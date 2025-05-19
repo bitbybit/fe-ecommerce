bcabdb0e47d5ea6c2483aed52d5c845089df994f
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-05-19 11:22:28 +0200

    feat: ECOM-40 reset auth error on login form load

--- app/pages/login/login-form/index.tsx
+++ app/pages/login/login-form/index.tsx
@@ -1,54 +1,67 @@
-import { useState, type ReactElement } from 'react'
+import { useState, type ReactElement, useEffect } from 'react'
+import { useNavigate } from 'react-router'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { type z } from 'zod'
+import { type FormType } from '~/utils/form'
 import { Button } from '~/components/ui/button'
 import { Form } from '~/components/ui/form'
-import { signIn, AUTH_STATUS } from '~/store/auth'
-import { useAppDispatch, useAppSelector } from '~/store/hooks'
-import { defaultValues } from './default-values'
-import { schema } from './schema'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/ui/card'
+import { ErrorAlert } from './error-alert'
 import { Email } from './fields/email'
 import { Password } from './fields/password'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/ui/card'
-import { LoginErrorAlert } from './login-error-alert'
-import { useNavigate } from 'react-router'
+import { signIn, AUTH_STATUS, resetAuthError } from '~/store/auth'
+import { useAppDispatch, useAppSelector } from '~/store/hooks'
+import { defaultValues } from './default-values'
+import { schema, type SchemaType } from './schema'
+
+const Fields = ({ form, status }: { form: FormType<SchemaType>; status: AUTH_STATUS }): ReactElement => {
+  return (
+    <fieldset disabled={status === AUTH_STATUS.LOADING}>
+      <CardContent>
+        <Email {...form} />
+        <Password {...form} />
+      </CardContent>
+      <CardFooter>
+        <Button type="submit">Submit</Button>
+      </CardFooter>
+    </fieldset>
+  )
+}
 
 export const LoginForm = (): ReactElement => {
-  const dispatch = useAppDispatch()
   const navigate = useNavigate()
-
+  const dispatch = useAppDispatch()
   const { status, errorMessage } = useAppSelector((state) => state.auth)
   const [isErrorMessageVisible, setIsErrorMessageVisible] = useState(false)
+
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues
   })
-  const handleLogin = (payload: z.infer<typeof schema>): void => {
-    dispatch(signIn(payload))
-      .unwrap()
-      .then(() => navigate('/', { replace: true }))
-      .catch(() => setIsErrorMessageVisible(true))
+
+  const handleLogin = async (payload: z.infer<typeof schema>): Promise<void> => {
+    try {
+      await dispatch(signIn(payload)).unwrap()
+      await navigate('/', { replace: true })
+    } catch {
+      setIsErrorMessageVisible(true)
+    }
   }
-  const handleFormChange = (): void => setIsErrorMessageVisible(false)
+
+  const handleChange = (): void => setIsErrorMessageVisible(false)
+
+  useEffect(() => void dispatch(resetAuthError()), [dispatch])
 
   return (
-    <Card>
+    <Card className="md:min-w-lg">
       <CardHeader>
         <CardTitle>LOGIN</CardTitle>
       </CardHeader>
-      {status === AUTH_STATUS.ERROR && isErrorMessageVisible && LoginErrorAlert(errorMessage)}
+      {status === AUTH_STATUS.ERROR && isErrorMessageVisible && ErrorAlert(errorMessage)}
       <Form {...form}>
-        <form onSubmit={(event) => void form.handleSubmit(handleLogin)(event)} onChange={handleFormChange}>
-          <CardContent>
-            <Email {...form} />
-            <Password {...form} />
-          </CardContent>
-          <CardFooter>
-            <Button type="submit" disabled={status === AUTH_STATUS.LOADING}>
-              Submit
-            </Button>
-          </CardFooter>
+        <form onSubmit={(event) => void form.handleSubmit(handleLogin)(event)} onChange={handleChange}>
+          <Fields form={form} status={status} />
         </form>
       </Form>
     </Card>
