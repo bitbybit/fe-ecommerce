eb4892c3b269d10fb568567d482298713480a178
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-05-25 22:52:34 +0400

    feat: ECOM-68 add product

--- app/api/namespaces/product.ts
+++ app/api/namespaces/product.ts
@@ -1,5 +1,9 @@
 import { ctpApiClient, type CtpApiClient } from '~/api/client'
-import { type ClientResponse, type ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk'
+import {
+  type ClientResponse,
+  type ProductProjectionPagedQueryResponse,
+  type ProductProjection
+} from '@commercetools/platform-sdk'
 
 type ProductApiProperties = {
   client: CtpApiClient
@@ -14,6 +18,10 @@ class ProductApi {
 
   public async getProducts(): Promise<ClientResponse<ProductProjectionPagedQueryResponse>> {
     return this.client.root.productProjections().get().execute()
+  }
+
+  public async getProductByID(): Promise<ClientResponse<ProductProjection>> {
+    return this.client.root.productProjections().withId({ ID: '424783fa-d6b2-40cb-97fd-1b5f7b4ee9fd' }).get().execute()
   }
 }
 
