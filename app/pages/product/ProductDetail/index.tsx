18eb395c4a04282bf1058e4c8788bd2d1be9af38
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-06-01 18:50:02 +0400

    fix: ECOM-68 rename file

--- app/pages/product/ProductDetail/index.tsx
+++ app/pages/product/ProductDetail/index.tsx
@@ -1,8 +1,8 @@
 import type { ReactElement } from 'react'
 import ProductDetail from './ProductDetail'
 import { Loading } from '~/components/Loading'
-import { useProductData } from '../hooks/useProductData1'
-import { PRODUCT_STATUS } from '../hooks/useProductData1'
+import { useProductData } from '../hooks/useProductData'
+import { PRODUCT_STATUS } from '../hooks/useProductData'
 
 export default function ProductCardContainer(): ReactElement {
   const { product, status } = useProductData()
--- app/pages/product/index.tsx
+++ app/pages/product/index.tsx
@@ -2,8 +2,8 @@ import { type ReactElement } from 'react'
 import { useTitle } from '~/hooks/useTitle'
 import ProductDetail from './ProductDetail/ProductDetail'
 import { Loading } from '~/components/Loading'
-import { useProductData } from './hooks/useProductData1'
-import { PRODUCT_STATUS } from './hooks/useProductData1'
+import { useProductData } from './hooks/useProductData'
+import { PRODUCT_STATUS } from './hooks/useProductData'
 
 export default function Product(): ReactElement {
   useTitle('Product')
