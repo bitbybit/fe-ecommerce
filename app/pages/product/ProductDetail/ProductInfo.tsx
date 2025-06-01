8507ce66a1822e578c84d474ee22e04a28ae8166
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-06-01 22:16:12 +0400

    feat: ECOM-68 delete loading string

--- app/pages/product/ProductDetail/ProductInfo.tsx
+++ app/pages/product/ProductDetail/ProductInfo.tsx
@@ -21,7 +21,7 @@ export function ProductInfo({
   return (
     <div className="flex-1 flex flex-col justify-start gap-4 text-left">
       <H1 className="text-2xl font-semibold">{name[LANG]}</H1>
-      <P className="text-muted-foreground">{description ? description[LANG] : 'Loading...'}</P>
+      <P className="text-muted-foreground">{description[LANG]}</P>
       {hasDiscount ? (
         <div className="flex items-baseline gap-2">
           <P className="line-through text-muted-foreground">{formatProductItemPrice(price)}</P>
