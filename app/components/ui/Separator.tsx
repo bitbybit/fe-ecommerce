5e2e6ea579ef13596a81305eb7a6a42643511fe3
Meruert <mika.amantaikuzu@gmail.com>
2025-06-03 14:42:34 +0500

    feat: ECOM-59 add Sidebar UI

---
+++ app/components/ui/Separator.tsx
@@ -0,0 +1,26 @@
+'use client'
+import * as SeparatorPrimitive from '@radix-ui/react-separator'
+import { cn } from '~/utils/ui'
+import type { ComponentProps, ReactElement } from 'react'
+
+function Separator({
+  className,
+  orientation = 'horizontal',
+  decorative = true,
+  ...properties
+}: ComponentProps<typeof SeparatorPrimitive.Root>): ReactElement {
+  return (
+    <SeparatorPrimitive.Root
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+        className
+      )}
+      {...properties}
+    />
+  )
+}
+
+export { Separator }
