a6be736b81b228b55794280a73aee41932237a60
Meruert <mika.amantaikuzu@gmail.com>
2025-06-15 20:02:46 +0500

    feat: ECOM-85 add product item quantity control

---
+++ app/pages/cart/QuantityControl.tsx
@@ -0,0 +1,35 @@
+import { Minus, Plus } from 'lucide-react'
+import type { ReactElement } from 'react'
+import { Button } from '~/components/ui/Button'
+
+type QuantityControlProperties = {
+  quantity: number
+  handleQuantityChange: (quantity: number) => void
+}
+
+export function QuantityControl({ quantity = 1, handleQuantityChange }: QuantityControlProperties): ReactElement {
+  const handleDecrease = (): void => {
+    if (quantity > 1) handleQuantityChange(quantity - 1)
+  }
+
+  const handleIncrease = (): void => {
+    handleQuantityChange(quantity + 1)
+  }
+
+  return (
+    <div className="flex gap-2">
+      <Button
+        variant="gray"
+        className="w-6 h-6 rounded-sm cursor-pointer"
+        onClick={handleDecrease}
+        disabled={quantity === 1}
+      >
+        <Minus />
+      </Button>
+      <div className="w-6 h-6 flex justify-center items-center">{quantity}</div>
+      <Button variant="gray" className="w-6 h-6 rounded-sm cursor-pointer" onClick={handleIncrease}>
+        <Plus />
+      </Button>
+    </div>
+  )
+}
