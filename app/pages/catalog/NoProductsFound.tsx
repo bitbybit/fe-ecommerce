804eb170b37370d6104f3e63c07ff20e8ab46486
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-06-17 00:03:44 +0400

    refactor: ECOM-91 change color theme in notFound files

--- app/pages/catalog/NoProductsFound.tsx
+++ app/pages/catalog/NoProductsFound.tsx
@@ -5,7 +5,7 @@ export function NoProductsFound(): ReactElement {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="max-w-xs flex flex-col items-center justify-center gap-4 text-center">
-        <Sparkles size={60} className="text-sky-200" />
+        <Sparkles size={60} className="text-neural-200" />
         <p className="text-sm">We couldn’t find any products matching your search.</p>
       </div>
     </div>
