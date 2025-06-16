2f27a144c2c0dbff88a111e096265c2c33145f82
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-06-16 16:43:33 +0400

    fix: ECOM-91 fix test

--- app/pages/product/ProductDetail/ProductInfo.test.tsx
+++ app/pages/product/ProductDetail/ProductInfo.test.tsx
@@ -1,16 +1,49 @@
-import { type LocalizedString } from '@commercetools/platform-sdk'
+import { type ProductProjection } from '@commercetools/platform-sdk'
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from '~/utils/test'
 import { ProductInfo } from './ProductInfo'
-
+const mockProductProjection: ProductProjection = {
+  id: 'product-id-1',
+  version: 1,
+  createdAt: new Date().toISOString(),
+  lastModifiedAt: new Date().toISOString(),
+  productType: { typeId: 'product-type', id: 'type-id' },
+  name: { 'en-US': 'Test Product' },
+  description: {
+    'en-US': 'Test description'
+  },
+  slug: { 'en-US': 'mock-product' },
+  masterVariant: {
+    id: 1,
+    prices: [
+      {
+        id: '1',
+        value: {
+          type: 'centPrecision',
+          currencyCode: 'USD',
+          centAmount: 1000,
+          fractionDigits: 2
+        },
+        key: 'price-usd'
+      }
+    ],
+    images: [
+      {
+        url: '/images/mock-product.jpg',
+        dimensions: { w: 500, h: 500 }
+      }
+    ]
+  },
+  variants: [],
+  hasStagedChanges: false,
+  published: true,
+  categories: []
+}
 describe('ProductInfo', () => {
-  const name: LocalizedString = { 'en-US': 'Test Product' }
-  const description: LocalizedString = { 'en-US': 'This is a great product.' }
-
   it('renders product name and description', () => {
-    renderWithProviders(<ProductInfo name={name} description={description} price={100} />)
+    renderWithProviders(<ProductInfo product={mockProductProjection} />)
 
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Test Product')
-    expect(screen.getByText('This is a great product.')).toBeInTheDocument()
+    expect(screen.getByText('Test Product')).toBeInTheDocument()
+    expect(screen.getByText((text) => text.includes('Test description'))).toBeInTheDocument()
   })
 })
